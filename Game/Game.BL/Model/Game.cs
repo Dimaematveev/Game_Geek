351dd446cf720406fb960e7762e4d4e227ebc82d
dimaematveev <dimaematveev@gmail.com>
2019-08-30 22:47:01 +0300

    Простейший класс Game готов! Simplest class Game is ready!

--- Game/Game.BL/Model/Game.cs
+++ Game/Game.BL/Model/Game.cs
@@ -8,5 +8,17 @@ namespace Game.BL.Model
 {
     public class Game
     {
+       
+        private User User { get; }
+
+        public Game(User user)
+        {
+            if (user==null)
+            {
+                throw new ArgumentNullException("Пользователь не может быть пустым!!", nameof(user));
+            }
+            User = user;
+        }
+
     }
 }
