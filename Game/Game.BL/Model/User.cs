625bcb0d0990f3796d28a71112867187b10ea699
dimaematveev <dimaematveev@gmail.com>
2019-08-30 22:44:13 +0300

    Простейший класс User готов! Simplest class User is ready!

--- Game/Game.BL/Model/User.cs
+++ Game/Game.BL/Model/User.cs
@@ -8,5 +8,19 @@ namespace Game.BL.Model
 {
     public class User
     {
+        private string Name {get; }
+        private int Count { get; set; }
+
+        public User(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пользователя не может быть пустым!!", nameof(name));
+            }
+
+            Name = name;
+            Count = 200;
+        }
+
     }
 }
