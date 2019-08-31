45bd1c6486768a63aa1d1822726cc8c275c85db9
dimaematveev <dimaematveev@gmail.com>
2019-08-31 10:24:55 +0300

    Add controller location with list location!

---
+++ Game/Game.BL/Controller/ControllerLocation.cs
@@ -0,0 +1,17 @@
+﻿using Game.BL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.BL.Controller
+{
+    public class ControllerLocation
+    {
+        List<Location> Locations;
+        Location CurrentLocation;
+
+
+    }
+}
