dcbeddf1a4b47436415a026a74d4db55b5261a13
dimaematveev <dimaematveev@gmail.com>
2019-09-06 06:49:54 +0300

    пустые тесты!

---
+++ Checkers_NEW/Checkers.BLTests/Model/SimpleHowMoveTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Checkers.BLTests.Model
+{
+    [TestClass]
+    public class SimpleHowMoveTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Checkers_NEW/Checkers.BLTests/Model/SimplePlayerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Checkers.BLTests.Model
+{
+    [TestClass]
+    public class SimplePlayerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
