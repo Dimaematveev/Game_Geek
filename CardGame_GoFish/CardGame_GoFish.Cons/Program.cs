ca8627024e2144ca3d02c3853cc544940b8c2d15
dimaematveev <dimaematveev@gmail.com>
2020-05-07 02:39:01 +0300

    Перевод на английский

--- CardGame_GoFish/CardGame_GoFish.Cons/Program.cs
+++ CardGame_GoFish/CardGame_GoFish.Cons/Program.cs
@@ -10,8 +10,8 @@ namespace CardGame_GoFish.Cons
     {
         static void Main(string[] args)
         {
-            #region Создал Достоинства карт
-            //Достоинства карт
+            #region Создал Достоинства карт. Create Cards Ranks
+            //Достоинства карт - Cards Ranks
             string[] cardsRanks = new string[13] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "V", "D", "K", "T" };
             Console.WriteLine("Достоинства карт:");
             Console.Write("\t");
@@ -22,8 +22,8 @@ namespace CardGame_GoFish.Cons
             Console.WriteLine();
             #endregion
 
-            #region Создал Колоду карт, не использовал масти
-            //Колода карт
+            #region Создал Колоду карт, не использовал масти. Create deck of Cards, but not use suits
+            //Колода карт - deck Of Cards
             List<string> deckOfCards = new List<string>();
             foreach (var item in cardsRanks)
             {
@@ -41,8 +41,8 @@ namespace CardGame_GoFish.Cons
             Console.WriteLine();
             #endregion
 
-            #region Создал Бассейн откуда игроки будут брать карты, это перемешанная колода. 
-            //Колода карт
+            #region Создал Бассейн откуда игроки будут брать карты, это перемешанная колода. Create pool, where players take card. Pool is shuffle deck of cards
+            //Бассейн - pool
             List<string> pool = ShuffleADeckOfCards(deckOfCards);
             Console.WriteLine("Бассейн:");
             Console.Write("\t");
@@ -53,20 +53,20 @@ namespace CardGame_GoFish.Cons
             Console.WriteLine();
             #endregion
 
-            #region Игроки с первоначальным кол-вом карт, и вывод оставшегося бассейна, Счет каждого игрока.
-            //Кол-во игроков
+            #region Игроки с первоначальным кол-вом карт, и вывод оставшегося бассейна, Счет каждого игрока. Players with the original number of cards, and withdrawal of the remaining pool.  each player’s score
+            //Кол-во игроков -  count Player
             int countPlayer = 2;
-            //Кол-во карт
+            //Кол-во карт - count Card
             int countCard = 7;
             if (countPlayer >= 5)
             {
                 countCard = 5;
             }
 
-            //Счет игроков
+            //Счет игроков - players Score
             int[] playersScore = new int[countPlayer];
 
-            // Карты игроков
+            // Карты игроков - players Cards
             List<string>[] playersCards = new List<string>[countPlayer];
             for (int i = 0; i < countPlayer; i++)
             {
@@ -83,10 +83,10 @@ namespace CardGame_GoFish.Cons
                 }
             }
 
-            Console.WriteLine($"Карты у игроков:");
+            Console.WriteLine($"Карты у игроков:"); //Cards of players
             for (int i = 0; i < countPlayer; i++)
             {
-                Console.WriteLine($"\tИгрок {i} Имеет карты:");
+                Console.WriteLine($"\tИгрок {i} Имеет карты:"); //Player i have cards
                 Console.Write("\t");
                 for (int j = 0; j < playersCards[i].Count; j++)
                 {
@@ -96,7 +96,7 @@ namespace CardGame_GoFish.Cons
             }
             Console.WriteLine();
 
-            Console.WriteLine("Оставшийся Бассейн:");
+            Console.WriteLine("Оставшийся Бассейн:"); // out pool
             Console.Write("\t");
             for (int i = 0; i < pool.Count; i++)
             {
@@ -105,7 +105,7 @@ namespace CardGame_GoFish.Cons
             Console.WriteLine();
             #endregion
 
-            #region Первоначальная проверка На 4 одинаковые карты
+            #region Первоначальная проверка На 4 одинаковые карты. Initial  check for 4 identical card
             for (int i = 0; i < countPlayer; i++)
             {
                 foreach (var item in playersCards[i].GroupBy(x => x).Where(x => x.Count() == 4))
@@ -117,7 +117,7 @@ namespace CardGame_GoFish.Cons
             }
             #endregion
 
-            #region ходы игроков
+            #region ходы игроков. Move player
             while (playersScore.Sum()<13)
             {
                 for (int i = 0; i < countPlayer; i++)
@@ -127,20 +127,20 @@ namespace CardGame_GoFish.Cons
             }
             #endregion
 
-            #region Победа
+            #region Победа. win
             int maxScore = playersScore.Max();
             var win = playersScore.Where(x => x.Equals(maxScore));
             if (win.Count() > 1) 
             {
-                Console.WriteLine("Победу разделили:");
+                Console.WriteLine("Победу разделили:");//Victory shared
             }
             else 
             {
-                Console.WriteLine("Выиграл:");
+                Console.WriteLine("Выиграл:");//won
             }
             foreach (var item in win)
             {
-                Console.WriteLine($"\tИгрок {item} со счетом {maxScore}!");
+                Console.WriteLine($"\tИгрок {item} со счетом {maxScore}!"); //Player item with score maxScore
             }
             
             #endregion
@@ -149,16 +149,16 @@ namespace CardGame_GoFish.Cons
         }
 
         /// <summary>
-        /// Один ход
+        /// Один ход. One move
         /// </summary>
-        /// <param name="playersScore">Счет игроков</param>
+        /// <param name="playersScore">Счет игроков. </param>
         /// <param name="playersCards"> Карты игроков</param>
         /// <param name="i">Номер текущего игрока</param>
         /// <param name="pool">Бассейн</param>
         private static void OneMove(int[] playersScore, List<string>[] playersCards, int i, List<string> pool)
         {
             Console.WriteLine($"Ход {i} Игрока!");
-            //ход остаётся у игрока?
+            //ход остаётся у игрока?. is Player Has Move
             bool isPlayerHasMove = false;
             string asksThatCard;
             int indPlayer;
@@ -169,30 +169,31 @@ namespace CardGame_GoFish.Cons
             }
             else
             {
-                Console.WriteLine("Вы не имеете карт, поэтому берете карту из бассейна");
+                Console.WriteLine("Вы не имеете карт, поэтому берете карту из бассейна");//You do not have a card, so take card from the pool
                 indPlayer = i;
                 asksThatCard = "-";
             }
-           
 
-            //Если такая карта имеется у другого игрока
+
+            //Если такая карта имеется у другого игрока. If such a card is have to another player.
             if (playersCards[indPlayer].Contains(asksThatCard))
             {
-                Console.WriteLine($"У игрока {indPlayer} имеется такая карта.");
+                Console.WriteLine($"У игрока {indPlayer} имеется такая карта.");// Player indPlayer have such a card
                 //Передача всех карт
                 playersCards[i].AddRange(playersCards[indPlayer].Where(x => x.Equals(asksThatCard)));
                 playersCards[indPlayer].RemoveAll(x=>x.Equals(asksThatCard));
                 isPlayerHasMove = true;
             }
-            //Если такой карты не имеется у другого игрока
+            //Если такой карты не имеется у другого игрока. If such a card is not have to another player.
             else
             {
-                Console.WriteLine($"У игрока {indPlayer} нет такой карты.");
+                Console.WriteLine($"У игрока {indPlayer} нет такой карты."); // Player indPlayer not have such a card
                 string pulledCard;
-                //Вытаскиваем из бассейна карту, если она равна той что нам надо вытаскиваем еще раз и т.д.
+                //Вытаскиваем из бассейна карту, если она равна той что нам надо вытаскиваем еще раз и т.д. 
+                //We take out the card from the pool, if it is equal to the one that we need to take out again
                 do
                 {
-                    //Бассейн пуст
+                    //Бассейн пуст - pool is Empty
                     if (pool.Count==0)
                     {
                         break;
@@ -200,21 +201,21 @@ namespace CardGame_GoFish.Cons
                     pulledCard = pool[0];
                     pool.RemoveAt(0);
                     playersCards[i].Add(pulledCard);
-                    Console.WriteLine($"Вы взяли из бассейна {pulledCard} .");
+                    Console.WriteLine($"Вы взяли из бассейна {pulledCard} .");//You take from pool ...
                 } while (pulledCard == asksThatCard);
             }
 
 
-            //Если их 4 то счет++ и сброс
+            //Если их 4 то счет++ и сброс. if you have 4 equal card then score++ and put cards
             foreach (var item in playersCards[i].GroupBy(x => x).Where(x => x.Count() == 4))
             {
                 playersScore[i]++;
-                Console.WriteLine($"Вы имеете 4 одинаковых карты {item.Key}. Теперь ваш счет {playersScore[i]}");
+                Console.WriteLine($"Вы имеете 4 одинаковых карты {item.Key}. Теперь ваш счет {playersScore[i]}");//if you have 4 equal card ... Now you score
                 playersCards[i].RemoveAll(x => x.Equals(item.Key));
             } 
            
 
-            Console.WriteLine($"Имеете Карты :");
+            Console.WriteLine($"Имеете Карты :");//Have cards
             Console.Write("\t");
             for (int j = 0; j < playersCards[i].Count; j++)
             {
@@ -230,13 +231,13 @@ namespace CardGame_GoFish.Cons
         }
 
         /// <summary>
-        /// Получить индекс игрока у которого будем мпрашивать
+        /// Получить индекс игрока у которого будем спрашивать. Get the index of the player we will ask
         /// </summary>
         /// <param name="countPlayer">Количество игроков</param>
         /// <param name="i">Номер текущего игрока</param>
         private static int GetIndPlayer(int countPlayer, int i)
         {
-            Console.WriteLine("Напишите номер игрока у которого хотите спросить из:");
+            Console.WriteLine("Напишите номер игрока у которого хотите спросить из:");// Write number of the player you want to ask 
             Console.Write("\t");
             for (int j = 0; j < countPlayer; j++)
             {
@@ -247,7 +248,7 @@ namespace CardGame_GoFish.Cons
             }
             Console.WriteLine();
             Console.WriteLine();
-            //Индекс игрока у которого спрашивается карта
+            //Индекс игрока у которого спрашивается карта. Index of the player you want to ask the card
             int indPlayer = -1;
             while (true)
             {
@@ -255,32 +256,32 @@ namespace CardGame_GoFish.Cons
                 {
                     if (indPlayer == i)
                     {
-                        Console.WriteLine("Вы не можете спросить у себя!!! Введите еще раз!!");
+                        Console.WriteLine("Вы не можете спросить у себя!!! Введите еще раз!!");//You can not ask yourself. Repeat  please
                         continue;
                     }
                     if (indPlayer < 0 || indPlayer >= countPlayer)
                     {
-                        Console.WriteLine("Нет такого игрока!!! Введите еще раз!!");
+                        Console.WriteLine("Нет такого игрока!!! Введите еще раз!!"); //There is no such player. Repeat  please
                         continue;
                     }
 
                     break;
                 }
-                Console.WriteLine("Это не число!!! Введите еще раз!!");
+                Console.WriteLine("Это не число!!! Введите еще раз!!");// This is not a number. Repeat  please
             }
             Console.WriteLine();
             return indPlayer;
         }
 
         /// <summary>
-        /// Получить Карту которую будем спрашивать
+        /// Получить Карту которую будем спрашивать. Get the card we will ask
         /// </summary>
-        /// <param name="playerCards">Карты текущего игрока</param>
+        /// <param name="playerCards">Карты текущего игрока.  current player Cards</param>
         private static string GetCardAsk(List<string> playerCards)
         {
-            Console.WriteLine("Напишите карту которую хотите спросить из:");
+            Console.WriteLine("Напишите карту которую хотите спросить из:");// Write the cards you want to ask 
             Console.Write("\t");
-            //Различные карты 
+            //Различные карты -             Various cards
             //TODO:Из групировки можно вытащить кол-во карт, но есть ли смысл?
             var variousCards = playerCards.GroupBy(x => x).Select(x=>x.Key).ToList();
             foreach (var card in variousCards)
@@ -289,7 +290,7 @@ namespace CardGame_GoFish.Cons
             }
             Console.WriteLine();
             Console.WriteLine();
-            //Карту которую спрашивает игрок
+            //Карту которую спрашивает игрок.             The card that the player asks
             string asksThatCard = "";
             while (true)
             {
@@ -299,17 +300,18 @@ namespace CardGame_GoFish.Cons
                     asksThatCard = rdl;
                     break;
                 }
-                Console.WriteLine("Нет такой карты у вас! Введите еще раз!!");
+                Console.WriteLine("Нет такой карты у вас! Введите еще раз!!");// You have not that card
             }
             Console.WriteLine();
             return asksThatCard;
         }
 
         /// <summary>
-        /// Перетасовка колоды карт. Входящая колода остается неизменной
+        /// Перетасовка колоды карт. Входящая колода остается неизменной. 
+        /// Shuffling a deck of cards.The incoming deck remains unchanged.
         /// </summary>
         /// <param name="deck">Колода</param>
-        /// <returns>Вывод перетасованной колоды</returns>
+        /// <returns>Вывод перетасованной колоды.</returns>
         private static List<string> ShuffleADeckOfCards(List<string> deck)
         {
             Random rnd = new Random();
